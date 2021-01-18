41e8768157a31e0a862389d8c54963769a933c95
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-19 00:15:01 +0300

    feat(App): create componet

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-export default function App() {
+import { Operation as DoctorsOperation } from "./reducer/doctors/doctors";
+import reducer from "./reducer/reducer";
+
+import Navigation from "./navigation";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+store.dispatch(DoctorsOperation.readDoctorsListFromStorage());
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
